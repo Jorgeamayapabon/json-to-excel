af8fc140d38e64dc56a58af7bda8cb1dc5d016e9
jamayapabon <jeap1723@gmail.com>
2023-09-09 20:16:08 -0500

    Added docstring

--- src/tool_manager.py
+++ src/tool_manager.py
@@ -5,18 +5,42 @@ from tools.constant import DATA_LITERAL_STR, FILENAME_JSON_STR, FILEPATH_STR
 
 
 def load_json_data() -> list:
+    """
+    Load data from a JSON file and return it as a list.
+
+    Returns:
+        list: A list of data loaded from the JSON file.
+    """
     with open(FILENAME_JSON_STR, "r") as json_file:
         data = json.load(json_file)
     return data
 
 
 def change_data_to_idx(data: list) -> list:
+    """
+    Change the index of each item in the data list.
+
+    Args:
+        data (list): The data list to which the index will be changed.
+
+    Returns:
+        list: The data list with changed indices.
+    """
     for index, item in enumerate(iterable=data, start=1):
         item[DATA_LITERAL_STR] = index - 1
     return data
 
 
 def extract_data_json(data: list) -> list:
+    """
+    Extract JSON data from a list of data.
+
+    Args:
+        data (list): The data list from which JSON data will be extracted.
+
+    Returns:
+        list: A list of JSON data extracted from the input list.
+    """
     data_json: list = []
     for item in data:
         json_loads = json.loads(item[DATA_LITERAL_STR])
@@ -26,6 +50,15 @@ def extract_data_json(data: list) -> list:
 
 
 def create_tables(data: list) -> None:
+    """
+    Create and save tables in an Excel file from a list of data.
+
+    Args:
+        data (list): The data list from which tables will be created.
+
+    Returns:
+        None
+    """
     table2 = pd.DataFrame(extract_data_json(data=data))
     table1 = pd.DataFrame(change_data_to_idx(data=data))
 
